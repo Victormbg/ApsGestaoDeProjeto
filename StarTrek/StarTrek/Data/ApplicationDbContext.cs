4e357acd01439854e6922d643ee654f8fb422c10
Roberto de Paiva Assumpção <robertopaiva26@outlook.com>
2022-11-12 20:35:38 -0300

    ApplicationDbContext
    Microsoft.EntityFrameworkCore

---
+++ StarTrek/StarTrek/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StarTrek.Models;
+
+namespace StarTrek.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Nave> Naves { get; set; }
+    }
+}
