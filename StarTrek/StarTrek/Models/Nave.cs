3fb8789dffe8831d942157307f0fc0e91bba0ce8
Roberto de Paiva Assumpção <robertopaiva26@outlook.com>
2022-11-12 19:34:03 -0300

    Nave model

---
+++ StarTrek/StarTrek/Models/Nave.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StarTrek.Models
+{
+    public class Nave
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        public string Afiliacao { get; set; }
+        public string Classe { get; set; }
+        public DateTime DataLancamento { get; set; }
+        public string Registro { get; set; }
+        public string Status { get; set; }
+        public string Propulsao { get; set; }
+        public string Armamento { get; set; }
+        public string Defesa { get; set;}
+
+
+    }
+}
