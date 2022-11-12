45d730bfe11737c0898f5c9de6b2c6752bdb6109
Roberto de Paiva Assumpção <robertopaiva26@outlook.com>
2022-11-12 20:50:09 -0300

    add-migration

---
+++ StarTrek/StarTrek/Migrations/20221112234842_AddNaveToDatabase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StarTrek.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNaveToDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Naves",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Afiliacao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Classe = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataLancamento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Registro = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Propulsao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Armamento = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Defesa = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Naves", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Naves");
+        }
+    }
+}
