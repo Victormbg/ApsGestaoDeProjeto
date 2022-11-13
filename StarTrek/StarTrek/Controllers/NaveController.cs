534513d607fbfdecfab186c9e6d0377c82848829
Roberto de Paiva Assumpção <robertopaiva26@outlook.com>
2022-11-12 22:35:06 -0300

    Tornando a variavel objlist nave fortemente tipada

--- StarTrek/StarTrek/Controllers/NaveController.cs
+++ StarTrek/StarTrek/Controllers/NaveController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using StarTrek.Data;
+using StarTrek.Models;
 
 namespace StarTrek.Controllers
 {
@@ -13,7 +14,7 @@ namespace StarTrek.Controllers
 
         public IActionResult Index()
         {
-            var objNaveList = _db.Naves.ToList();
+            IEnumerable<Nave> objNaveList = _db.Naves.ToList();
             return View();
         }
     }
