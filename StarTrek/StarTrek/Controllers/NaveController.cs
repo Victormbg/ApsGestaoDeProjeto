290efb2206354e8d447165078d30490a4893dc91
Roberto de Paiva Assumpção <robertopaiva26@outlook.com>
2022-11-13 18:23:16 -0300

    Adicionar nave e voltar lista

--- StarTrek/StarTrek/Controllers/NaveController.cs
+++ StarTrek/StarTrek/Controllers/NaveController.cs
@@ -17,5 +17,19 @@ namespace StarTrek.Controllers
             IEnumerable<Nave> objNaveList = _db.Naves.ToList();
             return View(objNaveList);
         }
+        // GET
+        public IActionResult Adicionar()
+        {
+            return View();
+        }
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Adicionar(Nave obj)
+        {
+            _db.Naves.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
