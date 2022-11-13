e32d135099ba1140730db8a7ee245d50f86f954e
Roberto de Paiva Assumpção <robertopaiva26@outlook.com>
2022-11-12 22:32:14 -0300

    Buscando todas as naves do banco de dados

--- StarTrek/StarTrek/Controllers/NaveController.cs
+++ StarTrek/StarTrek/Controllers/NaveController.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StarTrek.Data;
 
 namespace StarTrek.Controllers
 {
     public class NaveController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public NaveController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            var objNaveList = _db.Naves.ToList();
             return View();
         }
     }
